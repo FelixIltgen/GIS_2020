72b286d726dd1e41d78b215522209a4ae30e9238
Felix iltgen <felix.horst.iltgen@hs-furtwangen.de>
2020-06-30 17:08:36 +0200

    fin

--- Aufgabe_09/server.ts
+++ Aufgabe_09/server.ts
@@ -23,26 +23,14 @@ export namespace Aufgabe9 {
 
         _response.setHeader("content-type", "text/html; charset=utf-8");
         _response.setHeader("Access-Control-Allow-Origin", "*");
-        /*
-               if (_request.url) {
-                   let url: Url.UrlWithParsedQuery = Url.parse(_request.url, true);
-                   for (let key in url.query) {
-                       _response.write(key + ":" + url.query[key] + " <br/>");
-                   }
-       
-                   _response.write("$$$");
-                   let urlJson: string = JSON.stringify(url.query);
-       
-                   _response.write(urlJson);
-               }
-       */
+        
         if (_request.url) {
             let url: Url.UrlWithParsedQuery = Url.parse(_request.url, true);
 
             if (url.pathname == "/hbutton") {
 
                 for (let key in url.query) {
-                    //schreiben als html vertänlicher text
+                    //als HTML
                     _response.write(key + ":" + url.query[key] + " <br/>");
                 }
             } else if (url.pathname == "/jbutton") {
