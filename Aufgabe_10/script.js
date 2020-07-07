57d7670df371057c3d8326d951b27d31c2a30f10
Felix iltgen <felix.horst.iltgen@hs-furtwangen.de>
2020-07-07 15:50:40 +0200

    Aufgabe10

--- Aufgabe_10/script.js
+++ Aufgabe_10/script.js
@@ -4,23 +4,23 @@ var Aufgabe10;
     document.getElementById("senden")?.addEventListener("click", hinzufügen);
     document.getElementById("antwort")?.addEventListener("click", anzeigen);
     async function hinzufügen() {
-        console.log("sdfsdf");
         let formData = new FormData(document.forms[0]);
         let url = "https://gis2020felix.herokuapp.com";
         url += "/hinzufügen";
         // tslint:disable-next-line: no-any
         let query = new URLSearchParams(formData);
         url = url + "?" + query.toString();
+        console.log("sdfsdf");
         document.getElementById("data")?.reset();
         await fetch(url);
     }
     async function anzeigen() {
-        console.log("sdfsdf");
         let url = "https://gis2020felix.herokuapp.com";
         url += "/anzeigen";
         let response = await fetch(url, { method: "get" });
         let response2 = await response.text();
         document.getElementById("inhalt").innerHTML = response2;
+        console.log("sdfsdf");
     }
 })(Aufgabe10 || (Aufgabe10 = {}));
 //# sourceMappingURL=script.js.map
--- Aufgabe_10/script.ts
+++ Aufgabe_10/script.ts
@@ -4,25 +4,26 @@ namespace Aufgabe10 {
     document.getElementById("antwort")?.addEventListener("click", anzeigen);
 
     async function hinzufügen(): Promise<void> {
-        console.log("sdfsdf");
+        
         let formData: FormData = new FormData(document.forms[0]);
         let url: string = "https://gis2020felix.herokuapp.com";
         url += "/hinzufügen";
         // tslint:disable-next-line: no-any
         let query: URLSearchParams = new URLSearchParams(<any>formData);
         url = url + "?" + query.toString();
+        console.log("sdfsdf");
         (<HTMLFormElement>document.getElementById("data"))?.reset();
         await fetch(url);
     }
 
     async function anzeigen(): Promise<void> {
-        console.log("sdfsdf");
+        
         let url: string = "https://gis2020felix.herokuapp.com";
         url += "/anzeigen";
         let response: Response = await fetch(url,{method: "get"});
         let response2: string = await response.text(); 
         (<HTMLElement>document.getElementById("inhalt")).innerHTML = response2;
-        
+        console.log("sdfsdf");
     }
 
 
